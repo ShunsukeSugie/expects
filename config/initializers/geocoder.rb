a3690146fe5238f7e238e78bd31bf69543e28b08
ShunsukeSugie <psku3ixdae@gmail.com>
2019-08-30 22:49:17 +0900

    commit

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,7 @@
 Geocoder.configure(
   # street address geocoding service (default :nominatim)
   lookup: :google,
+<<<<<<< Updated upstream
 
   # IP address geocoding service (default :ipinfo_io)
   # ip_lookup: :maxmind,
@@ -14,4 +15,19 @@ Geocoder.configure(
   # set default units to kilometers:
   units: :km,
 
+=======
+  :http_proxy => 'rneh4uo8ry9dw:c5uc5kKCoF_aIYJyfxSSRQGO4w@http://quotaguard.com:9292',
+  # IP address geocoding service (default :ipinfo_io)
+  # ip_lookup: :maxmind,
+  language: :ja,
+  # to use an API key:
+  api_key: ENV['GEOCORDER'],
+  use_https: true,
+  # geocoding service request timeout, in seconds (default 3):
+  timeout: 5,
+
+  # set default units to kilometers:
+  units: :km,
+
+>>>>>>> Stashed changes
 )
