8402bafa70c9805391a8027cb42cb99f01076cec
ShunsukeSugie <psku3ixdae@gmail.com>
2019-08-30 21:55:44 +0900

    commit

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,17 +1,42 @@
-Geocoder.configure(
-  # street address geocoding service (default :nominatim)
-  lookup: :google,
+# Geocoder.configure(
+#   # street address geocoding service (default :nominatim)
+#   lookup: :google,
 
-  # IP address geocoding service (default :ipinfo_io)
-  # ip_lookup: :maxmind,
-  language: :ja,
-  # to use an API key:
-  api_key: ENV['GEOCORDER'],
-  use_https: true,
-  # geocoding service request timeout, in seconds (default 3):
-  timeout: 5,
+#   # IP address geocoding service (default :ipinfo_io)
+#   # ip_lookup: :maxmind,
+#   language: :ja,
+#   # to use an API key:
+#   api_key: ENV['GEOCORDER'],
+#   use_https: true,
+#   # geocoding service request timeout, in seconds (default 3):
+#   timeout: 5,
 
-  # set default units to kilometers:
-  units: :km,
+#   # set default units to kilometers:
+#   units: :km,
 
-)
+# )
+
+if Rails.env.production? then
+  Geocoder.configure(
+    lookup: :google,
+    always_raise: [
+      Geocoder::OverQueryLimitError,
+      Geocoder::RequestDenied,
+      Geocoder::InvalidRequest,
+      Geocoder::InvalidApiKey
+    ],
+    api_key: ENV['GEOCORDER'],
+    use_https: true
+  )
+  else
+  Geocoder.configure(
+    lookup: :google,
+    always_raise: [
+      Geocoder::OverQueryLimitError,
+      Geocoder::RequestDenied,
+      Geocoder::InvalidRequest,
+      Geocoder::InvalidApiKey
+    ],
+    api_key: Settings.google.map_key
+  )
+  end
