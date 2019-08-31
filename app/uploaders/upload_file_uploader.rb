50fa440f33643969d76f332f8d167889c449a75c
ShunsukeSugie <psku3ixdae@gmail.com>
2019-09-01 03:57:01 +0900

    commit

--- app/uploaders/upload_file_uploader.rb
+++ app/uploaders/upload_file_uploader.rb
@@ -38,7 +38,8 @@ class UploadFileUploader < CarrierWave::Uploader::Base
   # def extension_whitelist
   #   %w(jpg jpeg gif png)
   # end
-
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   # def filename
