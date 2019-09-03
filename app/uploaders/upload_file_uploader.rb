476504f302f7414f7da669a5a2c30a3fede50c44
ShunsukeSugie <psku3ixdae@gmail.com>
2019-09-03 13:47:47 +0900

    commit

--- app/uploaders/upload_file_uploader.rb
+++ app/uploaders/upload_file_uploader.rb
@@ -5,7 +5,13 @@ class UploadFileUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
 
-  storage :fog
+  if Rails.env.development?
+    storage :fog
+  elsif Rails.env.test?
+    storage :fog
+  else
+    storage :fog
+  end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
