1cf6379e495d4cf8e5012ca9734568a03780bfad
ShunsukeSugie <psku3ixdae@gmail.com>
2019-09-10 16:37:30 +0900

    commit

---
+++ spec/factories/user_profiles.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user_profile do
+    last_name       {"鈴木"}
+    first_name      {"友菜"}
+    last_name_kata  {"スズキ"}
+    first_name_kata {"ユウナ"}
+    birth           {"1995-01-01"}
+    tel             {"00000000000"}
+  end
+end
+
