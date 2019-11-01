797a16e04bce3547fbe99415d005f991faaf28ac
ShunsukeSugie <psku3ixdae@gmail.com>
2019-11-01 17:34:23 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module Expects
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    #　以下の記述を追記する(設定必須)
+    config.i18n.default_locale = :ja
     # the framework and any gems in your application.
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
   end
